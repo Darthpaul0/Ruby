7fb97d07531d2692c184d793806405b92c038267
Darthpaul0 <pablofr95@gmail.com>
2023-01-31 14:56:10 +0100

    Adding comments to codewar 5

--- Exercises/Codewar/Kata 5/codewar_5.rb
+++ Exercises/Codewar/Kata 5/codewar_5.rb
@@ -11,15 +11,25 @@ LINK --> https://www.codewars.com/kata/5503013e34137eeeaa001648
 =end
 
 def diamond(n)
+  # we won't accept even and negative numbers as inputs
   return nil if n.even? || n.negative?
 
+  # initialize diamond
   diamond_string = ""
+
+  # drawing process
   n.times do |i|
+    # get number of spaces
     space_count = (i - n / 2).abs
+
+    # get number of asterisks
     asterisk_count = n - space_count * 2
+
+    # build diamond
     diamond_string << [' ' * space_count, '*' * asterisk_count].join + "\n"
   end
 
+  # final result
   diamond_string
 end
 
